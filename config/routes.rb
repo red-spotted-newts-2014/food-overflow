b53f789694059310fb29f94fa88afb61b4fd11d6
Armen Vartan & Molly Huerster <pair+armenvartan.mhuerster@devbootcamp.com>
2014-08-08 17:45:39 -0400

    created routes for users and questions. shells for controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  root 'questions#index'
 
+  resources :users, shallow: true do
+    resources :questions
+  end
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
