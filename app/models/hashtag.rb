0bb0a324e2e0cf218a522c7b984abca350667165
Dylan Krause <dykrause@gmail.com>
2014-08-08 17:52:31 -0400

    Make Tagize Method Far Hashtags
    Tagize turns an input into a more tag friendly format before it is saved. Needs to be further refined.

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -4,4 +4,11 @@ class Hashtag < ActiveRecord::Base
 	has_many :hashtag_questions
 	has_many :questions, through: :hashtag_questions
 
+	before_save :tagize
+
+
+	private 
+	def tagize
+		title.downcase!.gsub!(" ", "-")
+	end 
 end
