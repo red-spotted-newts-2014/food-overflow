a481124be21b0d464c4539aa3fe02de32bc07614
Armen Vartan & Molly Huerster <pair+armenvartan.mhuerster@devbootcamp.com>
2014-08-08 18:09:38 -0400

    add test for questions controller, correct typo in factory

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 describe QuestionsController do
-  let(:question) { FactoryGirl.create :question}
+  let!(:question) { FactoryGirl.create :question }
+
+  it "#index" do
+    get :index
+    expect(assigns(:questions)).to eq Question.all
+  end
 end
