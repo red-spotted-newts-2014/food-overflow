895d5e2298f085dc941ddffb45feeaa3973cf1aa
Armen Vartan & Molly Huerster <pair+armenvartan.mhuerster@devbootcamp.com>
2014-08-09 16:29:11 -0400

    reformat destroy test - all tests pass

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -57,8 +57,10 @@ describe QuestionsController, type: :controller do
   end
 
   context "#destroy" do
-    xit "should delete a question" do
-      expect{ delete :destroy, id: question.id}.to change(Question, :count).by(-1)
+    it "should delete a question" do
+      expect{
+        delete :destroy, id: question.id
+        }.to change(Question, :count).by(-1)
     end
   end
 end
