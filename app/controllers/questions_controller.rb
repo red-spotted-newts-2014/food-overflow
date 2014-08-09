28c0eec86ff5f70fe46ed4aa7845a6e1e306939c
mhuerster <mary.huerster@gmail.com>
2014-08-09 13:51:45 -0400

    fixed weird nil/each thing. this is a bad commit. sorry.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,8 @@
 class QuestionsController < ApplicationController
   
-  def index
-    @questions = Question.all
-  end
+  	def index
+	    @questions = Question.all
+	  end
 
   def show
     @question = Question.find(params[:id])
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140808160431) do
+ActiveRecord::Schema.define(version: 20140809010608) do
 
   create_table "comments", force: true do |t|
     t.text     "content"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20140808160431) do
     t.string   "image_url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "access_token"
   end
 
   create_table "votes", force: true do |t|
