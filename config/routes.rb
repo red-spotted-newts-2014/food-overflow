f502decfd86fedfb26303224a5f252ebd7c7f4bd
mhuerster <mary.huerster@gmail.com>
2014-08-09 14:17:02 -0400

    uncomment login routes - I think this works

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   
   root 'questions#index'
 
-  # root 'questions#show'
-
   get '/users/sign_in', to: 'users#sign_in'
   post '/users/auth', to: 'users#auth'
   get '/users/auth', to: 'users#landing_page'
