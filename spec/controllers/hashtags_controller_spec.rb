c7929bdba8d4fce584ed81c63f7ad9349d27cc71
Dylan Krause <dykrause@gmail.com>
2014-08-09 19:47:36 -0400

    Update Hashtags Spec

--- spec/controllers/hashtags_controller_spec.rb
+++ spec/controllers/hashtags_controller_spec.rb
@@ -3,15 +3,25 @@ require 'rails_helper'
 describe HashtagsController do 
 	let!(:hashtag) {FactoryGirl.create :hashtag }
 
-	it "#index" do 
-		get :index 
-		expect(assigns(:hashtags)).to eq Hashtag.all
-	end
+		context "#index" do 
+			it "provides the view with all hashtags" do
+				get :index 
+				expect(assigns(:hashtags)).to eq Hashtag.all
+			end
+		end
 
-	it "#show" do 
-		pending
-		# get :show, id: :hashtag
+		context "#show" do 
+			it "shows one hashtag object" do 
+				get :show, id: hashtag.id
+				 expect(assigns(:hashtag)).to eq Hashtag.find hashtag.id
+	    end
+		end
 
-	end
+		# context "#questions" do 
+		# 	it "lists all questions tagged by a hashtag" do 
+		# 		get :questions, id: hashtag.id
+		# 		expect(assigns()).to eq Hashtag.find(hashtag.id).questions
+		# 	end
+		# end
 
 	end
