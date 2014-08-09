9a24ed69e61a9f5892c144cc7f5531a6c2a860af
Armen Vartan & Molly Huerster <pair+armenvartan.mhuerster@devbootcamp.com>
2014-08-09 17:15:24 -0400

    add test for clicking on question link - passing

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -3,11 +3,17 @@ feature "User browsing FoodOverflow" do
 
   context "view all questions" do
     let!(:user) { FactoryGirl.create :user }
-    let!(:post) { FactoryGirl.create :post }
+    let!(:question) { FactoryGirl.create :question }
 
     it "sees a list of recent question titles" do
       visit root_path
-      expect(page).to have_content("#{post.title}")
+      expect(page).to have_content("#{question.title}")
+    end
+
+    it "can click on a question title and see the question page" do
+      visit root_path
+      click_link("#{question.title}")
+      expect(current_path). to eq(question_path id: question.id)
     end
   end
 end
