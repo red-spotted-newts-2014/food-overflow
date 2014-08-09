81bac1774475b654e8444be946ecef22849dea4d
Dylan Krause <dykrause@gmail.com>
2014-08-09 16:23:15 -0400

    Establish Basic Hastag Table Format

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,7 +1,7 @@
 class HashtagsController < ApplicationController
 
 	def index 
-		@hashtags = Hashtag.all
+		@hashtags = Hashtag.all.sort_by{|tag| tag.title}
 		@hashtags_tables = @hashtags.each_slice(3).each_slice(5).to_a
 	end
 
