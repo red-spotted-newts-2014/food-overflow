ffe27e3d67a43e612fa7cffb6941e235aa0ef4f3
mhuerster <mary.huerster@gmail.com>
2014-08-09 19:37:48 -0400

    write spec for create method

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,1 +1,13 @@
 require 'rails_helper'
+
+describe CommentsController do
+	let!(:question) { FactoryGirl.create :question }
+
+	context "#create" do
+		it "should create a comment with valid parameters" do
+			expect{
+				post :create, comment: { content: "bacon ipsum dolor sit amet", commenable_id: question.id, commentable_type: "Question" }
+			}.to change (Comment, :count).by(1)
+		end
+	end
+end
