dfb6122f3e8630b8c775383b87350db2ce80d28e
mhuerster <mary.huerster@gmail.com>
2014-08-08 22:54:43 -0400

    add methods for login, show single user, logout

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+
+	def show
+		@users = User.all
+	end
+
+	def sign_in
+	  @secret = ENV['CLIENT_ID']
+	  @state="somenonsense"
+	  redirect_to "https://github.com/login/oauth/authorize?client_id=935c620d7555e425f1bf&redirect_uri=http://127.0.0.1:3000/users/auth&scope=user&state=#{@state}"
+	end
+
+	def auth
+	  @code = params[:code]
+	  state = params[:state]
+	  redirect_to "https://github.com/login/oauth/access_token?client_id=#{ENV['CLIENT_ID']}&client_secret=#{ENV['CLIENT_SECRET']}&code=@code&redirect_uri=http://127.0.0.1:3000/auth" if state == @state
+	end
+
+	def landing_page
+	  @access_token = params[:access_token]
+	  @user = User.find_or_create_by(access_token: @access_token)
+	  session[:user_id] = @user.id
+	  redirect_to root_url
+	end
+
+	def logout
+	  session[:user_id] = nil
+	  redirect_to root_url
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:password, :password_confirmation)
+	end
+end
