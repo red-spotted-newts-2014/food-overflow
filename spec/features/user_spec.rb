6925cb63eae5103a9f8d1ff695478fe3cc9c9b06
Armen Vartan & Molly Huerster <pair+armenvartan.mhuerster@devbootcamp.com>
2014-08-09 17:06:37 -0400

    write first feature test

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,7 +1,13 @@
+require 'rails_helper'
 feature "User browsing FoodOverflow" do
 
   context "view all questions" do
     let!(:user) { FactoryGirl.create :user }
     let!(:post) { FactoryGirl.create :post }
-    
+
+    it "sees a list of recent question titles" do
+      visit root_path
+      expect(page).to have_content("#{post.title}")
+    end
+  end
 end
