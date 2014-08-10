911a773b6b98a180a7b47d1b5d91208d22e23c59
armenvartan <vartan.armen@gmail.com>
2014-08-09 21:59:01 -0400

    scope for count_downvotes. passes test

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,5 @@ class Vote < ActiveRecord::Base
 	belongs_to :votable, polymorphic: :true
 
 	scope :count_upvotes, -> { where(is_upvote?: true).count }
+	scope :count_downvotes, -> { where(is_upvote?: false).count }
 end
