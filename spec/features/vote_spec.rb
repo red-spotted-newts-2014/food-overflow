89a07ba44b8942bbdd67768f1c6374973da4ff77
armenvartan <vartan.armen@gmail.com>
2014-08-10 16:44:21 -0400

    fixed tests to work

--- spec/features/vote_spec.rb
+++ spec/features/vote_spec.rb
@@ -3,12 +3,14 @@ require 'rails_helper'
 describe "vote box" do
   context "upvote" do
     it "comes back to the votes path" do
+      visit votes_path
       click_on "upvote"
       expect(current_path).to eq(votes_path)
     end
 
     it "creates an upvote" do
-      expect{click_on "upvote"}.to change(Vote.count_upvotes).by(1)
+      visit votes_path
+      expect{click_on "upvote"}.to change{ Vote.count_upvotes }.by(1)
     end
   end
 end
