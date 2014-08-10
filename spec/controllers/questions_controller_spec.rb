74664489b5014508e9542442181e1db6137a8e0c
mhuerster <mary.huerster@gmail.com>
2014-08-10 14:18:24 -0400

    add tests for flash notice on question creation

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -16,10 +16,20 @@ describe QuestionsController, type: :controller do
         }.to change(Question, :count).by(1)
     end
 
+    it "should generate a flash notice on successful creation" do
+      post :create, question: { title: "Steak", content: "Tartare" }
+      expect(flash[:notice]).to eq("Your question has been posted.")
+    end
+
     it "should NOT create a question with invalid params" do
       expect{
         post :create, question: { title: "", content: "" }
       }.not_to change(Question, :count)
+    end
+
+    it "should generate a flash notice on unsuccessful creation" do
+      post :create, question: { title: "", content: "" }
+      expect(flash[:notice]).to eq("Something was wrong. Please try again.")
     end
   end
 
