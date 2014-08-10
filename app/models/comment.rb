c05659acaed4d8cd5c5921d086a06bad59d7844f
mhuerster <mary.huerster@gmail.com>
2014-08-09 22:22:08 -0400

    add length validation to comment content

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,5 +5,5 @@ class Comment < ActiveRecord::Base
 	has_many :comments, as: :commentable
 	belongs_to :commentable, polymorphic: :true
 	has_many :votes, as: :votable
-
+	validates_length_of :content, minimum: 1
 end
