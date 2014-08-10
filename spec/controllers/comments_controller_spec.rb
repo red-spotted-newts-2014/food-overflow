3bd010a2f8f24f8d664e16c047991b8b495bcfbf
mhuerster <mary.huerster@gmail.com>
2014-08-09 22:21:04 -0400

    add test for invalid comment parameters

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -9,5 +9,10 @@ describe CommentsController do
 				post :create, comment: { content: "bacon ipsum dolor sit amet", commentable_id: question.id, commentable_type: "Question" }
 			}.to change(Comment, :count).by(1)
 		end
+		it "should not create a comment with invalid parameters" do
+			expect{
+				post :create, comment: { content: "", commentable_id: question.id, commentable_type: "Question" }
+			}.not_to change(Comment, :count)
+		end
 	end
 end
