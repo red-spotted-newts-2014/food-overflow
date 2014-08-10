1c8b3b59a8452afc67508350c3af5e60907f5b76
armenvartan <vartan.armen@gmail.com>
2014-08-09 22:17:59 -0400

    shells for vote controller and spec

--- spec/controllers/votes_controller.rb
+++ spec/controllers/votes_controller.rb
@@ -1,1 +1,17 @@
 require 'rails_helper'
+
+describe VotesController, type: :controller do
+  it "should create an upvote" do
+    let!(:vote1) { question.votes.create!(is_upvote?: true) }
+
+  end
+
+  it "should create an downvote" do
+    let!(:vote2) { question.votes.create!(is_upvote?: false) }
+
+  end
+
+  it "should show the vote difference" do
+
+  end
+end
