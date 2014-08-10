b0774a6270ae1753d1930b3c35ccc8bfe43f2a89
armenvartan <vartan.armen@gmail.com>
2014-08-10 14:15:22 -0400

    hopefully this is a better test for testing the upvote button

--- spec/features/vote_spec.rb
+++ spec/features/vote_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 describe "vote box" do
-  it "creates an upvote" do
-    click_on "upvote"
-    expect(current_path).to eq(votes_path)
+  context "upvote" do
+    it "comes back to the votes path" do
+      click_on "upvote"
+      expect(current_path).to eq(votes_path)
+    end
+
+    it "creates an upvote" do
+      expect{click_on "upvote"}.to change(Vote.count_upvotes).by(1)
+    end
   end
 end
