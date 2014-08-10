2724cb648aadaef565840f9f63be91dfd7c054e3
armenvartan <vartan.armen@gmail.com>
2014-08-10 13:41:43 -0400

    added context #create to create tests in votes controller spec

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
 describe VotesController, type: :controller do
+  context "#create" do
+    it "should create an upvote" do
+      expect{ post :create, vote: { is_upvote?: true }}
+      .to change(Vote.count_upvotes).by(1)
+    end
 
-  it "should create an upvote" do
-    expect{ post :create, vote: { is_upvote?: true }}
-    .to change(Vote.count_upvotes).by(1)
-  end
-
-  it "should create an downvote" do
-    expect{ post :create, vote: { is_upvote?: false }}
-    .to change(Vote.count_downvotes).by(1)
+    it "should create an downvote" do
+      expect{ post :create, vote: { is_upvote?: false }}
+      .to change(Vote.count_downvotes).by(1)
+    end
   end
 end
