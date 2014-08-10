c967cb96414aad1e20b961675124fdba0fc11ec9
mhuerster <mary.huerster@gmail.com>
2014-08-10 13:36:23 -0400

    create test for redirect after incorrect params

--- spec/features/comment_spec.rb
+++ spec/features/comment_spec.rb
@@ -9,5 +9,23 @@ feature 'Comments' do
 				visit question_url(question)
 				expect(page).to have_selector('form')
 		end
+
+		it 'successfully creates a comment with valid params' do
+			visit question_url(question)
+			within ('form') do
+				fill_in('Content', with: 'Lorem bacon dolor sit amet etc.')
+			end
+			click_button 'Create Comment'
+			expect(page).to have_content('Lorem bacon dolor sit amet etc.')
+		end
+
+		it "redirects to index instead of creating a comment with invalid params" do
+			visit question_url(question)
+			within ('form') do
+				fill_in('Content', with: '')
+			end
+			click_button 'Create Comment'
+			expect(current_path).to eq(root_path)
+		end
 	end
 end
