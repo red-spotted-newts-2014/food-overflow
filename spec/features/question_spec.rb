e7d7f7c383e6fb50656dee8e461d5176e9c99764
armenvartan <vartan.armen@gmail.com>
2014-08-09 20:24:42 -0400

    changed test on line 19 to BDD

--- spec/features/question_spec.rb
+++ spec/features/question_spec.rb
@@ -5,16 +5,27 @@ feature "Questions" do
 
   context "#index" do
     let!(:question) { FactoryGirl.create :question }
-    it "contains posts" do
+
+    it "contains questions" do
       visit questions_path
       expect(page).to have_content("All Questions")
       expect(page).to have_content(question.title)
     end
+  end
 
-    it "can click a link to the question#show" do
+  context "#show" do
+    let!(:question) { FactoryGirl.create :question }
+
+    it "it accessible from questions#index" do
       visit questions_path
       click_on question.title
-      expect(page).to have_content("yoyo") # will need to adjust to show view
+      expect(current_path).to eq question_path(question)
+    end
+
+    it "should show the title and content of a question" do
+      visit questions_path(question)
+      expect(page).to have_content(question.title)
+      expect(page).to have_content(question.content)
     end
   end
 end
