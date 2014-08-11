f0aba55244f49ac0de2cd68763ab2bbd086789ef
mhuerster <mary.huerster@gmail.com>
2014-08-11 09:02:42 -0400

    vote tests pass - good this time

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -6,13 +6,13 @@ describe VotesController, type: :controller do
     it "should create an upvote" do
       expect{
         question.votes << Vote.create(is_upvote: true)
-      }.to change(question.votes, :count).by(1)
+      }.to change(Vote, :count_upvotes).by(1)
     end
 
     it "should create a downvote" do
       expect{ 
         question.votes << Vote.create(is_upvote: false)
-    }.to change(question.votes, :count).by(1)
+    }.to change(Vote, :count_downvotes).by(1)
     end
   end
 end
