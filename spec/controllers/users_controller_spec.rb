19e674f15de582b62c3376bf4f684b88523e057e
armenvartan <vartan.armen@gmail.com>
2014-08-10 21:31:53 -0400

    test for user#new

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,9 +1,16 @@
 require 'rails_helper'
 
 describe UsersController, type: :controller do
-  context "#create" do
-    let!(:user) { FactoryGirl.create :user }
+  let!(:user) { FactoryGirl.create :user }
 
+  context "#new" do
+    it "assigns a new user" do
+      get :new
+      expect(assigns(:user)).to be_a_new User
+    end
+  end
+
+  context "#create" do
     it "creates a user" do
       expect{ post :create, user: { name: "Baconator", email: "bacon@bacon.com", password: "Bacon1234" }}
       .to change(User, :count).by(1)
