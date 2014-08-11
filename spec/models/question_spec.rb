92e3e9c97ee5807380a0525e060580f3dfcd0b41
mhuerster <mary.huerster@gmail.com>
2014-08-10 22:53:20 -0400

    add context to test info method

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,13 +1,20 @@
 require 'rails_helper'
 
+
 describe Question do
-  context 'associations' do
-    it { should belong_to :user }
-    it { should have_many :comments }
-    it { should have_many :votes }
-    it { should have_many :hashtag_questions }
-    it { should have_many :hashtags }
-  end
+let!(:question) { FactoryGirl.create :question }
+let!(:hashtag) { FactoryGirl.create :hashtag, question }
+  
+context 'associations' do
+	it { should belong_to :user }
+	it { should have_many :comments }
+	it { should have_many :votes }
+	it { should have_many :hashtag_questions }
+	it { should have_many :hashtags }
+end
+
+context '#info' do
+end
 
   
 end
