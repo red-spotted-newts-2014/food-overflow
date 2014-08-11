3f68c0d002a4c8f5cdf9ca9f04bd4270801b7c7c
mhuerster <mary.huerster@gmail.com>
2014-08-11 08:38:34 -0400

    working on getting user data from github api - not working but also not breaking

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,10 +15,29 @@ class UsersController < ApplicationController
 	def auth
 	  code = params[:code]
 	  state = params[:state]
-	  redirect_to "https://github.com/login/oauth/access_token?client_id=#{@client_id}&client_secret=#{ENV['CLIENT_SECRET']}&code=#{code}&redirect_uri=http://127.0.0.1:3000/auth" if state == @state
+	  @token_req = HTTParty.post("https://github.com/login/oauth/access_token",
+	  {:client_id => @client_id,
+	  :client_secret => ENV['CLIENT_SECRET'],
+	  :code => code},
+	  :accept => :json)
+	  @access_token = JSON.parse(@token_req)['access_token']
+	  scopes = JSON.parse(@token_req)['scope'].split(',')
+	  has_user_email_scope = scopes.include?('user:email')
+	  auth_result = JSON.parse(HTTParty.get("https://api.github.com/user", {params: {access_token: access_token}}))
+	  if has_user_email_scope
+	  	@email = auth_result['private_emails'] = JSON.parse(HTTParty.get("https://api.github.com/user/emails", {params: {access_token: access_token}}))
+	  end
+	  
+
+	  # redirect_to "https://github.com/login/oauth/access_token?client_id=#{@client_id}&client_secret=#{ENV['CLIENT_SECRET']}&code=#{code}" if state == @state
+
 	end
 
 	def landing_page
+		puts "*****"
+	  	p @access_token
+	  	p @email
+	  	puts "*****"
 	  @access_token = params[:access_token]
 	  @user = User.find_or_create_by(access_token: @access_token)
 	  session[:user_id] = @user.id
