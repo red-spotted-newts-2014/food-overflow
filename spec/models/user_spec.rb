5d0f8323c2c463e876efd906fcb1d9e3b88ce95b
mhuerster <mary.huerster@gmail.com>
2014-08-10 22:52:17 -0400

    add context to test info method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe User do
+	let!(:user) { User.new(name: "first last", email: "first.last@gmail.com", access_token: "test-token") }
 
 	context 'associations' do
 		it { should have_many :questions }
@@ -8,10 +9,12 @@ describe User do
 	end
 
 	context 'validations' do
-		let!(:user) { User.new(name: "first last", email: "first.last@gmail.com", access_token: "test-token") }
 		it "initially has no password if created via oauth" do
 			expect(user.password).to be_nil
 		end
+	end
+
+	context '#check_password' do
 		it "creates a dummy password for a user created via oauth" do
 			user.save
 			expect(user.password).to eq("gh-test-token")
