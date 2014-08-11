2b8db44557dfdfd4b13fda108f0c348de976b40c
armenvartan <vartan.armen@gmail.com>
2014-08-10 21:24:36 -0400

    spec for user creation

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,1 +1,12 @@
 require 'rails_helper'
+
+describe UsersController, type: :controller do
+  context "#create" do
+    let!(:user) { FactoryGirl.create :user }
+
+    it "creates a user" do
+      expect{ post :create, user: { user }}
+      .to change(User, :count).by(1)
+    end
+  end
+end
