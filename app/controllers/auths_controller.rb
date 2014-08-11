b044ba1c6780a87fff77e7af8324216a61e1f4a0
armenvartan <vartan.armen@gmail.com>
2014-08-11 13:36:26 -0400

    cleaning up code

--- app/controllers/auths_controller.rb
+++ app/controllers/auths_controller.rb
@@ -4,12 +4,10 @@ class AuthsController < ApplicationController
     redirect_to root_path
   end
 
-  def index #checking to see if github return is good to here
-    p params["code"]
+  def index
     @access_token = params["code"]
     @user = User.find_or_create_by(access_token: @access_token)
     session[:user_id] = @user.id
-    p @user
     redirect_to root_url
   end
 end
