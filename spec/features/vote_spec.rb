814e58db94d0e16a59812c8d9363a6e677d4e0b1
mhuerster <mary.huerster@gmail.com>
2014-08-11 09:50:57 -0400

    working on vote spec

--- spec/features/vote_spec.rb
+++ spec/features/vote_spec.rb
@@ -1,17 +1,21 @@
 require 'rails_helper'
 
 describe "vote box" do
+
+  let!(:question) { FactoryGirl.create :question }
+
   context "upvote" do
     it "comes back to the votes path" do
-      visit votes_path
+      visit question_url(question)
       click_on "upvote"
       expect(current_path).to eq(votes_path)
     end
 
     it "creates an upvote" do
-      visit votes_path
+      visit question_url(question)
       expect{
-        click_on "upvote"
+        find(:css, "div.voter.up").click
+        question.reload
       }.to change(Vote, :count_upvotes).by(1)
     end
   end
