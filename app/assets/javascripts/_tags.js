fa6798db429abfb870069a55c63b6a6b5af6f133
Dylan Krause <dykrause@gmail.com>
2014-08-11 02:13:48 -0400

    valueshower function

--- app/assets/javascripts/_tags.js
+++ app/assets/javascripts/_tags.js
@@ -6,6 +6,11 @@
 
 // }; 
 
+
+//Take Input 
+//Send to Relay 
+//Send to Dive Maker
+//Trigger Relay on submit question: to ajax
 var oneWordAjax = function (url, word) {
 				$.ajax({
 				url: url,
@@ -14,7 +19,7 @@ var oneWordAjax = function (url, word) {
 				data: { key : word},
 			})
 			.done(function() {
-				console.log("Sent ");
+				console.log("Sent");
 			})
 			.fail(function() {
 				console.log("error");
@@ -38,10 +43,16 @@ TagRelay.prototype.sendAll = function(url){
 	};
 }
 
-function ValueSender (location) {
+function ValueShower (location) {
 		this.location = location
 }
 
+ValueShower.prototype.append = function(value, el, className){
+	location.append("<"+el+" class="+className+">"+value+"</"+el+">")
+} 
+
+
+function ValueForm
  $(document).ready(function() {
 	console.log("done")
 	$(".tag_input").on('keyup', function(event) {
