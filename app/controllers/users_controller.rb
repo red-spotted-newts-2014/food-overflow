85c3c7f0979f76301187b28d1464c88dce1a1a25
armenvartan <vartan.armen@gmail.com>
2014-08-10 23:03:15 -0400

    added gems and added molly's starter code for oauth. going to try to complete it.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,25 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def sign_in
+	  @client_id = ENV['CLIENT_ID']
+	  @state="somenonsense"
+	  redirect_to "https://github.com/login/oauth/authorize?client_id=#{@client_id}&redirect_uri=http://127.0.0.1:3000/users/auth&scope=user&state=#{@state}"
+	end
+
+	def auth
+	  code = params[:code]
+	  state = params[:state]
+	  redirect_to "https://github.com/login/oauth/access_token?client_id=#{@client_id}&client_secret=#{ENV['CLIENT_SECRET']}&code=#{code}&redirect_uri=http://127.0.0.1:3000/auth" if state == @state
+	end
+
+	def landing_page
+	  @access_token = params[:access_token]
+	  @user = User.find_or_create_by(access_token: @access_token)
+	  session[:user_id] = @user.id
+	  redirect_to root_url
+	end
+
 	def show
 		@user = User.find_by(params[:id])
 	end
