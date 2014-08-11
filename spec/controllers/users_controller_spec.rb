152aeced3731cd7162ac8d3c65b2a88e3dd26480
armenvartan <vartan.armen@gmail.com>
2014-08-10 21:27:24 -0400

    fixed test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,7 +5,7 @@ describe UsersController, type: :controller do
     let!(:user) { FactoryGirl.create :user }
 
     it "creates a user" do
-      expect{ post :create, user: { user }}
+      expect{ post :create, user: { name: "Baconator", email: "bacon@bacon.com", password: "Bacon1234" }}
       .to change(User, :count).by(1)
     end
   end
