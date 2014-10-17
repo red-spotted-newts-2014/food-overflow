99d332faca68dedf51f31b9fda60ea9672b66126
Dylan Krause <dykrause@gmail.com>
2014-10-16 23:39:18 -0400

    Add basic outline to Backbone question view -define template -empty events -empty initialize -empty render

---
+++ app/assets/javascripts/global/views/questions.js
@@ -0,0 +1,14 @@
+app = app || {};
+
+app.QuestionView = Backbone.View.extend({
+
+  template: _.template( $('#question_template').html() ),
+
+  events: {},
+
+  initialize: function(){},
+
+  render: function(){}
+
+
+});
