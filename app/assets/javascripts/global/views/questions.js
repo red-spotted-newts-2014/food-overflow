676d52a532ad1500a420a78f636fac706fbdf2af
Dylan Krause <dykrause@gmail.com>
2014-10-18 19:09:10 -0400

    Add selector properties to Backbone question view initialization and write function for ajax submission of model

--- app/assets/javascripts/global/views/questions.js
+++ app/assets/javascripts/global/views/questions.js
@@ -13,12 +13,19 @@ app.QuestionView = Backbone.View.extend({
 
   initialize: function(){
     this.initializeTemplates();
+    this.$title = $(".new_question");
+    this.$content = $(".new_content");
+    this.$form = $(".question_form");
+    this.$show = $(".formulated_question")
+    this.model = new app.Question();
   },
 
   render: function(){},
 
   submitQuestion: function(e){
     e.preventDefault()
+    this.model.save({title: this.$title.val(), content: this.$content.val()})
+    console.log('geez')
   }
 
 });
