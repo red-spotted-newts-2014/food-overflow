002fe50937a133a3c54df96c3d24d1d93567ea49
Dylan Krause <dykrause@gmail.com>
2014-10-19 11:08:22 -0400

    Add new, create, and logout routes to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,23 @@
 class SessionsController < ApplicationController
 
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      flash[:notice] = "Invalid login!"
+      render :new
+  end
+
+  def logout
+    session.clear
+    flash[:notice] = "Signed out!"
+    redirect_to root_path
+  end
+
 end
