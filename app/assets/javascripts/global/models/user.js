1d27758ea8a848a58ee7da2e8649f132c72ce11e
Dylan Krause <dykrause@gmail.com>
2014-10-21 22:12:50 -0400

    Add potential defaults to user model -these aren't meant to sync up with db, but be representative of a user's relation to the current page

--- app/assets/javascripts/global/models/user.js
+++ app/assets/javascripts/global/models/user.js
@@ -1,4 +1,15 @@
 app = ap || {};
 
 app.User = Backbone.Model.extend({
+
+  defaults: {
+    user_id: null,
+    voteData = null,
+    questionOwner = null
+  },
+
+  initialize: function(){
+
+  },
+
 })
