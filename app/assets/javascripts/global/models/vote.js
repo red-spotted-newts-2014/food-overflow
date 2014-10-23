258d8bcf5bd7744ced6ca87149cdd8f14875601d
Dylan Krause <dykrause@gmail.com>
2014-10-22 23:23:29 -0400

    Add defaults to backbone vote model

--- app/assets/javascripts/global/models/vote.js
+++ app/assets/javascripts/global/models/vote.js
@@ -2,4 +2,14 @@ var app = app || {};
 
 app.Vote = Backbone.Model.extend({
 
+  defaults: {
+    user_id: null,
+    upvote: null,
+    vote_type: null,
+    vote_for: null
+  },
+
+  initialize: function(){
+
+  }
 })
