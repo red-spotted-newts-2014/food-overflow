094d5da9c4a36675a47b84508c8c15b42ee37829
Dylan Krause <dykrause@gmail.com>
2014-10-25 17:01:07 -0400

    add template initializer to vote collections view and initialize in initializer

--- app/assets/javascripts/global/views/votes.js
+++ app/assets/javascripts/global/views/votes.js
@@ -2,4 +2,16 @@ app = app || {};
 
 app.VoteView = Backbone.View.extend({
 
+  events: {
+  },
+
+  initializeTemplates: function(){
+    _.templateSettings = { interpolate: /\{\{(.+?)\}\}/g };
+    this.voterTemplate = _.template( $('#vote-template').html() )
+  },
+
+  initialize: function(){
+    this.initializeTemplates();
+  },
+
 });
