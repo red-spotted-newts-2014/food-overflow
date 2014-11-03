ce53433e9813e00075ebe229fdcbf2fd7137c52c
Dylan Krause <dykrause@gmail.com>
2014-11-03 14:38:17 -0500

    Add presence validations to vote model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,8 @@
 class Vote < ActiveRecord::Base
-belongs_to :votable, polymorphic: :true
-belongs_to :user
+  belongs_to :votable, polymorphic: :true
+  belongs_to :user
+
+  validates :user, :votable_type, :votable_id, presence: true
 
   scope :count_upvotes, -> { where(is_upvote: true).count }
   scope :count_downvotes, -> { where(is_upvote: false).count }
