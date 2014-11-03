95e17856a248380d72b321f34d76264d30ef10ac
Dylan Krause <dykrause@gmail.com>
2014-11-02 20:01:33 -0500

    Remove old spaghetti code for voting and delete corresponding vote controller create details

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,18 +1,6 @@
 class VotesController < ApplicationController
+
   def create
-    owner = params[:vote]["owner"]
-    id = owner[2,owner.size].to_i
-    upvote = params[:vote]["is_upvote"] == "true"
-    if owner[0] == "q"
-      @question = Question.find(id).votes.create(is_upvote: upvote)
-    respond_to do |format|
-      format.json {render json: @question.id.to_json}
-    end
-    end
-    if owner[0] == "c"
-      Comment.find(id).votes.create(is_upvote: upvote)
-    end
-    ""
   end
 
   def update
