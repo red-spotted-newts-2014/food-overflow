8da2d1fdb64870a3b1f76c9c1252e619ad423910
Dylan Krause <dykrause@gmail.com>
2014-11-03 11:59:02 -0500

    Remove bulky upvote/downvote syntax for backbone vote model and replace it with syncing; Remove nullvote method

--- app/assets/javascripts/global/models/vote.js
+++ app/assets/javascripts/global/models/vote.js
@@ -3,53 +3,22 @@ var app = app || {};
 app.Vote = Backbone.Model.extend({
 
   defaults: {
-    user_id: null,
     is_upvote: null,
-    vote_type: null,
-    vote_for: null
+    votable_type: null,
+    votable_id: null
   },
+
+  urlRoot: '/votes',
 
   initialize: function(){
 
   },
 
-  upVote: function(specs){
-    var self = this;
-    var userVote = {
-      "user_id": specs.user || null,
-      "votable_type": specs.ownerType || null,
-      "votable_id": specs.ownerId || null,
-      "is_upvote": true
-    };
-    $.ajax({
-      url: '/votes',
-      type: 'POST',
-      data: {vote: userVote}
-    })
-    .success(function(data) {
-      this.set(data)
-    })
-    .fail(function() {
-      console.log("error");
-    })
+  upVote: function(){
+    this.save('is_upvote', true)
   },
 
   downVote: function(){
-     var self = this;
-    $.ajax({
-      url: '/votes',
-      type: 'POST'
-    })
-    .success(function(data) {
-      this.set(data)
-    })
-    .fail(function() {
-      console.log("error");
-    })
-
-  },
-
-  nullVote: function(){
-
+    this.save('is_upvote', false)
   }
 })
